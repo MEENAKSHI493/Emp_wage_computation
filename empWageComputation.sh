ffebbbb1a7c6af5f6b5d5f2594a2faef5433221b
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-05 18:16:09 +0530

    employee part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,17 @@
-wel come employee
+  
+Welcome to employee wage computation
 
 x=$((RANDOM%2))
 if [[ $x -eq 1 ]];then
 echo employee is present
-else 
+else
 echo employee is absent
 fi
 
+fulldayhour=8
+parttimehour=4
+wagephour=20
 
-fulldhr=8
-wageperhr=20
-echo Fullday Employee Wage is- "$(($fulldhr*$wageperhr))"
+echo wage for fullday: "$(($fulldayhour*$wagephour))"
+
+echo part time wage  : "$(($parttimehour*$wagephour))"
